29974dbc1ec749a90ace4304650fc38bad7f62de
Léo Riera <leo.riera1@gmail.com>
2022-07-20 17:41:12 +0200

    events wait for selector

--- src/events.js
+++ src/events.js
@@ -26,6 +26,7 @@ const { getEvents, postEvent, postEventImage } = require('./request');
 
   for (const e of events) {
     await page.goto(`https://facebook.com/events/${e.facebookId}`);
+    await page.waitForSelector('h2 span[class=""]');
     await page.waitForTimeout(1000);
     const { dates, title, image } = await retrieveEventData(page);
     if (dates !== null && title !== null) {
